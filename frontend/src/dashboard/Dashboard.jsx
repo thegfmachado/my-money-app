34daf355df795cffc7c2e59c22ee089c75f894ff
1GabrielMachado1 <gabriel.ferreira.machado@hotmail.com>
2020-06-28 22:22:57 -0300

    Desenvolvimento do componente dashboard

--- frontend/src/dashboard/Dashboard.jsx
+++ frontend/src/dashboard/Dashboard.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/ContentHeader'
 import Content from '../common/template/Content'
+import ValueBox from '../common/widget/ValueBox'
+import Row from '../common/layout/Row'
 
 class Dashboard extends Component {
     render() {
@@ -9,7 +11,16 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title="Dashboard" subTitle="v1.0" />
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox cols="12 4" color="green" icon="bank"
+                            value="R$ 10" text="Total de Créditos" />
+
+                        <ValueBox cols="12 4" color="red" icon="credit-card"
+                            value="R$ 10" text="Total de Débitos" />
+
+                        <ValueBox cols="12 4" color="blue" icon="money"
+                            value="R$ 0" text="Valor Consolidado" />
+                    </Row>
                 </Content>
             </div>
         )
